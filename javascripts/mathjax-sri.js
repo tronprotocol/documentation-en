09d61843d2f08b02eeb0fff68e6fb7941d1f32e0
King31T <kingtower310303@gmail.com>
2026-05-08 10:24:00 +0800

    Rebuild docs: restructure API sections, add tip-workflow and versioned releases (#592)
    - Split api/http, api/json-rpc, api/rpc into per-endpoint subsections
    - Replace releases/history with releases/versions/ per-version pages
    - Add developers/tip-workflow page
    - Regenerate site assets (sitemap, search index, javascripts)

---
+++ javascripts/mathjax-sri.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+    var script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-MML-AM_CHTML';
+    script.integrity = 'sha384-crwIf/BuaWM9rM65iM+dWFldgQ1Un8jWZMuh3puxb8TOY9+linwLoI7ZHZT+aekW';
+    script.crossOrigin = 'anonymous';
+    script.async = true;
+    document.head.appendChild(script);
+})();
